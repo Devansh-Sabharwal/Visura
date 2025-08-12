05d042d353a1ced53606d06a25187e7da6b6ce8a
Devansh Sabharwal <sabharwaldevansh04@gmail.com>
2025-08-12 18:56:12 +0530

    added vercel analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Inter } from "next/font/google";
-
+import { Analytics } from "@vercel/analytics/next";
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -53,6 +53,7 @@ export default function RootLayout({
         <ReactQueryProvider>
           <Providers>
             {children}
+            <Analytics />
             <Toaster position="top-right" />
           </Providers>
         </ReactQueryProvider>
